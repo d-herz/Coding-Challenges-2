5f089d17c5a13e98ed9dc3db452f86de359a8cd1
Daniel Hyres <danhyres@gmail.com>
2022-05-13 17:31:35 -0400

    Grasshopper - Messi Goals Function

---
+++ grasshopper-messi-goals-function/main.js
@@ -0,0 +1,27 @@
+// 5-13-22
+
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+
+
+//P: goals league 1, goals league 2, goals league 3
+//R: sum of goals from all 3 leagues
+//E: goals(5,10,2) === 17
+//P: summation of the 3 variables
+
+
+function goals (l1, l2, l3) {
+  return l1+l2+l3
+}
