80c50062115269fcb06e460ebbdf431b7ca9a964
Daniel Hyres <danhyres@gmail.com>
2022-05-13 17:29:12 -0400

    Grasshopper-Variable Assignment Debug

---
+++ grasshopper-variable-assignment-debug/main.js
@@ -0,0 +1,11 @@
+// 5-13-22
+
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+//5-13-22
+
+var a = "dev"
+var b = "Lab"
+
+var name = `${a}${b}`
