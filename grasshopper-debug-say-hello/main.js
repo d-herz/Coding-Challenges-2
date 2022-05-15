4be847ecea842956c17266e7b2092705fb474b0b
Daniel Hyres <danhyres@gmail.com>
2022-05-15 10:44:03 -0400

    Grasshopper Debug Say Hello

---
+++ grasshopper-debug-say-hello/main.js
@@ -0,0 +1,12 @@
+// 5-15-22
+
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+
+function sayHello(name) {
+  return `Hello, ${name}`
+}
