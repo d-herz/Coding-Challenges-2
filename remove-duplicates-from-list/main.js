67e2ed5ec8fc594ba616417bf06ff24bac5c60d1
Daniel Hyres <danhyres@gmail.com>
2022-05-15 10:52:04 -0400

    Remove Duplicates from List

---
+++ remove-duplicates-from-list/main.js
@@ -0,0 +1,11 @@
+// 5-15-22
+
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+
+function distinct(a) {
+  let b = [...new Set(a)]
+  return b;
+}
