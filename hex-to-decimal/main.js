c1734c1a0d720749f1632122f01d134cea8a1c76
Daniel Hyres <danhyres@gmail.com>
2022-05-19 18:03:29 -0400

    hex to decimale

---
+++ hex-to-decimal/main.js
@@ -0,0 +1,9 @@
+// 5-19-22
+
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+//5-19-22
+
+function hexToDec(hexString){
+  return parseInt(hexString, 16)
+}
