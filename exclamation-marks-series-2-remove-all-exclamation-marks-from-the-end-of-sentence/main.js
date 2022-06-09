36571ac23c22f35c020660559a0a4dadcd65b775
Daniel Hyres <danhyres@gmail.com>
2022-06-09 12:15:02 -0400

    First Sol

---
+++ exclamation-marks-series-2-remove-all-exclamation-marks-from-the-end-of-sentence/main.js
@@ -0,0 +1,19 @@
+// Description:
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+
+function remove (string) {  
+  let str = string
+  
+  while(str[str.length-1]==='!'){
+    str = str.slice(0,-1)
+  }
+  return str
+}
