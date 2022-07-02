acd0f54d93814072363e1f1e58f98be07e14ccac
Daniel Hyres <danhyres@gmail.com>
2022-07-02 11:35:13 -0400

    Training JS 18

---
+++ training-js-18-methods-of-string-object-concat-split-join/main.js
@@ -0,0 +1,21 @@
+// Task
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+// For example:
+
+// splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+// splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+function splitAndMerge(str, sep) {
+  let arr = str.split(' ')
+  console.log(arr)
+  
+  let arr2 = arr.map( x => x.split('').join(sep))
+  console.log(arr2.join(' '))
+  return arr2.join(' ')
+   
+}
