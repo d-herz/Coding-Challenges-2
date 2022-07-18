55f6af3d3c97a14ba726766bb4505bd8469caeee
Daniel Hyres <danhyres@gmail.com>
2022-07-18 16:46:22 -0400

    My head is at the wrong end

---
+++ my-head-is-at-the-wrong-end/main.js
@@ -0,0 +1,35 @@
+/* <h1> My head is at the wrong end! </h1>
+
+<h2> Codewars 8Kyu 4-11-22 </h2>
+
+<h3> Redo: 5-2-22 </h2>
+
+<p>
+  
+  
+  You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+Simples!
+  
+</p> */
+
+
+//7-18-22
+function fixTheMeerkat(arr) {
+  return arr.reverse()
+ }
+ 
+ //5-2-22 Same sol but tried new syntax
+ const fixTheMeerkat = arr => arr.reverse()
+ 
+ 
+ //4-11-22 sol
+ function fixTheMeerkat(arr) {
+   return arr.reverse()
+ }
+ 
+ //FT!
