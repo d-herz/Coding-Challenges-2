43406ebbc8713b62cf2252b5c2d47811ad9dac92
Daniel Hyres <danhyres@gmail.com>
2022-07-18 16:44:57 -0400

    You only need one

---
+++ you-only-need-one-beginner/main.js
@@ -0,0 +1,23 @@
+/* <h1> You only need one - Beginner </h1>
+
+<h2> Codewars 8Kyu 4-11-22 </h2>
+<h3> Redo: 5-2-22 </h3>
+
+<p>
+  You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+</p> */
+
+//7-18-22
+function check(a, x) {
+  return a.includes(x)
+}
+
+
+//EZ FIRST T!! 4-11-22
+function check(a, x) {
+  return a.includes(x)
+}
