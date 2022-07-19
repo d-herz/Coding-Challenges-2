2c99bb3dc23ebe376774211f105362401e068b0c
Daniel Hyres <danhyres@gmail.com>
2022-07-19 11:53:29 -0400

    Swap Values

---
+++ swap-values/main.js
@@ -0,0 +1,24 @@
+/* <h1> Swap Values </h1>
+
+<h2> Codewars 8kyu 4-13-22 </h2>
+
+<p>
+  
+  I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+Can you figure out what's wrong here?
+  
+</p> */
+
+//7-19-22
+//Had to peek at my old sol
+function swapValues(arr) {
+  return arr.reverse()
+}
+
+
+//4-13-22
+
+function swapValues(arr) {
+   return arr.reverse()
+}
