6703469c0acce493570c24ee3d9720a018bed822
Daniel Hyres <danhyres@gmail.com>
2022-07-27 11:44:35 -0400

    jennys secret message

--- jennys-secret-message/main.js
+++ jennys-secret-message/main.js
@@ -1,4 +1,14 @@
 
+//7-27-22 EZ
+function greet(name){
+
+  if(name === "Johnny"){
+    return "Hello, my love!";
+
+  }else{
+      return "Hello, " + name + "!";
+  }
+}
 
 //5-3-22 solu
 //could use ternary next time
