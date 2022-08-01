a09972c2eefb09abf687a706a757c960e5a692c0
Daniel Hyres <danhyres@gmail.com>
2022-08-01 17:55:00 -0400

    Cheesing it

--- multiplication-table-for-number/main.js
+++ multiplication-table-for-number/main.js
@@ -1,4 +1,4 @@
-// Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+// Your goal is to return multiplication table for number that is always an integer from 1 to 10. not more.
 
 // For example, a multiplication table (string) for number == 5 looks like below:
 
@@ -15,7 +15,7 @@
 // P. S. You can use \n in string to jump to the next line.
 
 // Note: newlines should be added between rows, but there should be no trailing newline at the end. If you're unsure about the format, look at the sample tests.
-
+//I did great?
 
 function multiTable(number) {
   let str = ``
