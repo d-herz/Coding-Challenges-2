680ff7c7b4b280693dd92672f56d25197b24547c
Daniel Hyres <danhyres@gmail.com>
2022-08-24 11:50:14 -0400

    new sol

---
+++ reversed-string/main.js
@@ -0,0 +1,22 @@
+// CodeWars 8kyu Kata, 3/23/22
+
+// Complete the solution so that it reverses the string passed into it.
+
+
+//8-24-22
+function solution(str){
+  return str.split('').reverse().join('')
+  
+}
+
+
+//3-23-22 My baddie solution
+function solution(str) {
+    let splitString = str.split ('')
+    //split method turns string into array
+    let reverseArray = splitString.reverse()
+    //reverse array method reverses new array
+    let joinArray = reverseArray.join('')
+    //join method joins the elements of the array into a string
+    return joinArray
+}
