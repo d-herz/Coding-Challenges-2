7dd8df213cc690a4ca4beb79a96164136cb04245
Daniel Hyres <danhyres@gmail.com>
2022-08-24 11:52:33 -0400

    new sol

---
+++ remove-first-and-last/main.js
@@ -0,0 +1,44 @@
+/* <h1> Remove First and Last </h1>
+
+<h2>CodeWars 8Kyu Kata, 3/24/22</h2>
+
+<p>
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+redid 4/3/22 and pushed to GitHub
+  
+  Redid again 4-12-22
+  4-19-22 again
+</p> */
+
+//8-24-22 almost got got
+
+function removeChar(str){
+  return str.slice(1,-1)
+};
+
+//7-13-22
+function removeChar(str){
+  return str.slice(1,-1)
+};
+
+
+
+//4-19-22
+function removeChar(str){
+  return str.slice(1,-1)
+};
+
+
+//Earlier response
+function removeChar(str){
+  let result = str.slice(1,-1)
+ 
+  return result
+
+}
+
+
+// 4-3-22
+// return str.slice(1,-1)
+// 4-12-22 same ans return str.slice(1,-1)
