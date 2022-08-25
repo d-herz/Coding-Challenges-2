be87feb771b7d6ec41bfe86808e2dffa6c1f3af7
Daniel Hyres <danhyres@gmail.com>
2022-08-25 11:50:44 -0400

    sentence

--- sentence-smash/main.js
+++ sentence-smash/main.js
@@ -5,6 +5,29 @@
 // ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 
 
+//8-25-22
+function smash (words) {
+  return words.join(' ')
+};
+
+
+//basically same sol 4-22-22
+function smash (words) {
+  return words.join(' ').trim()
+};
+
+//my solution 3/31/22, didn't need trim I guess?
+function smash (words) {
+  return words.join(' ').trim()  
+};
+
+function smash (words) {
+ let arr = words.join(' ')
+  return arr
+};
+
+
+
 //7-10-22
 function smash (words) {
   return words.join(' ')
