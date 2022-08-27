3211cbcaa870704c3e057f79f8e7ce01ec1d5bdb
Daniel Hyres <danhyres@gmail.com>
2022-08-27 10:45:49 -0400

    repeat

--- convert-boolean-values-to-strings-yes-or-no/main.js
+++ convert-boolean-values-to-strings-yes-or-no/main.js
@@ -1,6 +1,12 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 
+//8-27-22
+
+function boolToWord( bool ){
+  return bool === true ? 'Yes' : 'No'
+ }
+
 function boolToWord( bool ){
   
   return bool === true ? "Yes" : 'No'
