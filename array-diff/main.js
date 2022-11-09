2ed5749a8d70353a2cafee5badf0c79d40e8653f
Daniel Hyres <danhyres@gmail.com>
2022-11-09 16:01:32 -0500

    updated arrayDiff sol

--- array-diff/main.js
+++ array-diff/main.js
@@ -23,3 +23,28 @@ function arrayDiff(a, b) {
 console.log( arrayDiff( [1,2], [1] ), '[2]' )
 console.log( arrayDiff( [1,2,2], [1] ), '[2,2]' )
 console.log( arrayDiff( [1,2,2], [2] ), '[1]' )
+
+//Other User Sol's
+
+// function arrayDiff(a, b) {
+//   b = new Set(b)
+//   return a.filter(v => !b.has(v))
+// }
+
+
+// function arrayDiff(a, b) {
+
+//   var arr = new Array(); //Not sure why you "new Array()" and not just []
+  
+//   for(var i = 0;i<a.length;i++){ //b.indexOf(a[i] < 0 ) because if not in b, indexOf returns -1
+//       if(b.indexOf(a[i])<0){
+//           arr.push(a[i]);
+//       }
+//   }
+
+//   return arr;
+// }
+
+// function arrayDiff(a, b) {
+//   return a.filter( v => { return b.indexOf(v) === -1 });
+// }
