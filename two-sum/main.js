e43e0596026f1c716ca6fc5e43611c950fe093cc
Daniel Hyres <danhyres@gmail.com>
2022-12-16 09:39:12 -0500

    Updated Two Sum sol with O(n) solution

--- two-sum/main.js
+++ two-sum/main.js
@@ -6,32 +6,51 @@
 
 // You can return the answer in any order.
 
- 
-
 // Example 1:
-
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
 
+// Example 2:
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
-// Example 3:
 
+// Example 3:
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
  
+// 10 - 28 - 22
 
-var twoSum = function(nums, target) {
+// var twoSum = function(nums, target) {
 
-  for(let i = 0; i < nums.length; i++){
+//   for(let i = 0; i < nums.length; i++){
 
-    for (let j = i + 1; j < nums.length; j++){
-     if (nums[i] + nums[j] === target) {
-      return [i, j]
-     } 
+//     for (let j = i + 1; j < nums.length; j++){
+//      if (nums[i] + nums[j] === target) {
+//       return [i, j]
+//      } 
+//     }
+//   }
+// };
+
+// 12-16-22
+
+var twoSum = function (numsArr, target) {
+
+  let numMap = {}
+  let pairs = []
+
+  for (const num of numsArr) {
+    num2 = target - num
+    if (numMap[num2]) {
+      pairs.push(num, num2)  
+    } else {
+      numMap[num]=1
     }
+  }
+  if (pairs.length = 2) {
+    console.log(pairs)
+    return [numsArr.indexOf(pairs[0]), numsArr.lastIndexOf(pairs[1])]
   }
 };
 
