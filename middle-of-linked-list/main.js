2d4930f0a135ea8f4899b52f53381c9471a24cab
Daniel Hyres <danhyres@gmail.com>
2023-02-28 17:30:31 -0500

    Leetcode: Middle of Linked List

---
+++ middle-of-linked-list/main.js
@@ -0,0 +1,33 @@
+// Standup 02-27-2023
+// Leetcode: Middle of the Linked List
+// https://leetcode.com/problems/middle-of-the-linked-list/
+
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+
+var middleNode = function (head) {
+  // Loop through the list until .next is no longer true (which would be at the tail)
+  // The fast and slow algorithm
+  slow = fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
+
